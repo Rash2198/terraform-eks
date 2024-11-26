cf71b6e6fdbfcb5ae09a1fc5f012603a8b47a25d
Rash2198 <149450181+Rash2198@users.noreply.github.com>
2024-11-26 12:30:32 +0530

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -20,12 +20,17 @@ module "eks" {
   }
 
   eks_managed_node_groups = {
-
     node_group = {
       min_size     = 1
       max_size     = 2
       desired_size = 2
+
+      labels = {
+        "environment" = "production"
+        "app"         = "my-app"
+      }
     }
   }
 }
+
 
